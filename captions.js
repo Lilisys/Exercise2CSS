20d69c6e453a633a4f658542af4a6b37345a7335
Lilisys <jesswu@umich.edu>
2016-06-14 22:23:18 -0400

    finished modal window

---
+++ captions.js
@@ -0,0 +1,33 @@
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+console.log(span)
+
+function modalOpen(text, source){
+	modal.style.display='block';
+	var newImg = document.createElement("img");
+	document.getElementById('modal-text').innerHTML = text;
+	document.getElementsByClassName('modal-img')[0].appendChild(newImg);
+	newImg.src = source;
+}
+
+//Delete the newly created image elt so 
+function clearImg(){
+  var delImg = document.getElementsByClassName('modal-img')[0]
+	console.log(delImg);
+	delImg.removeChild(delImg.firstChild);
+}
+
+//When you click the x, the modal window closes
+span.onclick = function() {
+  modal.style.display='none';
+  clearImg();
+}
+
+//When you click outside the modal window, it closes
+window.onclick = function(event){
+	if(event.target == modal){
+		modal.style.display='none'
+		console.log("clicked outside")
+		clearImg();
+	}
+}
